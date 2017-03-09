222473973cc5440c9ecd477193a1419995f3fa2a
Jonghwan Hyeon <hyeon0145@gmail.com>
2017-03-10 06:36:15 +0900

    Refactor codes

---
+++ namuwiki/_syntax.py
@@ -0,0 +1,108 @@
+import re
+
+_patterns = {
+    'macro': [
+        r'\[\[(?P<name>date|datetime|br|include|목차|tableofcontents|각주|footnote|pagecount|youtube|nicovideo|wikicommons)(?:\(.+?\))?\]\]', # rigvedawiki syntax
+        r'\[(?P<name>date|datetime|br|include|목차|tableofcontents|각주|footnote|pagecount|youtube)(?:\(.+?\))?\]',
+        r'\[\[(?P<name>분류)\:.*?\]\]',# acts like macro
+    ],
+
+    'html': [
+        r'\{\{\{\#\!html[ \t\n]+(?P<text>[\s\S]*?)\}\}\}',
+        r'\[\[html[ \t]*\((?P<text>[^\)]*)\)\]\]', # rigvedawiki syntax
+    ],
+
+    'image': [
+        r'\[\[파일\:[ \t]*.*?\.(?:jpg|jpeg|png|gif)(?:\|[\&a-z\=\d\%]*)?\]\]',
+        r'(?:https?|ftp).*?\.(?:jpg|jpeg|png|gif)(?:[\?\&a-z\=\d\%]*)?',
+        r'attachment\:[ \t]*[\"\']?.*?\.(?:jpg|jpeg|png|gif)(?:[\?\&a-z\=\d\%]*)?[\"\']?', # rigvedawiki syntax
+    ],
+
+    'link': [
+        # [[문서|출력]]
+        r'\[\[[^\|\]]*\|(?P<text>[^\]]*)\]\]',
+        # [[\#문서]]
+        r'\[\[\\(?P<text>\#[^\]]*?)(?:\#[^\]]*)?\]\]',
+        # [[문서#s-2.3]]
+        r'\[\[(?P<text>[^\#][^\]]*?)(?:\#[^\]]*)?\]\]',
+        # [[#s-1]]
+        r'\[\[\#(?P<text>[^\]]*)\]\]', 
+    ],
+
+    'horizontal_rule': r'-{4,10}',
+    'heading': r'={1,}[ \t]+(?P<text>.*?)[ \t]+\={1,}',
+    
+    'bold': r"'''(?P<text>.*?)'''",
+    'italic': r"''(?P<text>.*?)''",
+    'deletion': r'(~~|--)(?P<text>.*?)\1',
+    'underline': r'__(?P<text>.*?)__',
+    'superscript': r'\^\^(?P<text>.*?)\^\^',
+    'subscript': r',,(?P<text>.*?),,',
+    'text_size': r'\{\{\{\+\d+[ \t]+(?P<text>.*?)\}\}\}',
+    'text_color': r'\{\{\{\#(?:[0-9a-f]+|[a-z]+)[ \t]+(?P<text>.*?)\}\}\}',
+
+    'math': [
+        r'<math>.*?</math>',
+        r'\$.*?\$', # rigvedawiki syntax
+    ],
+
+    'text_box': r'\{\{\|(?P<text>[\s\S]*?)\|\}\}',
+    'table': r'\|.*?\|(?:<[^>]*>)*(?P<text>[^\|]*)',
+
+    'unordered_list': r'^[ \t]+\*[ \t]*(?P<text>.*)$',
+    'ordered_list': r'^[ \t]+[1AaIi]\.[ \t]*(?:\#\d+)?[ \t]*(?P<text>.*)$',
+    'quote': r'\>+[ \t]*(?P<text>.*)',
+
+    'syntax': r'\{\{\{\#\!syntax[ \t\n]+[\s\S]*?\}\}\}',
+    'wiki': r'\{\{\{\#\!wiki[ \t\n]+style[ \t]*\=[ \t]*\".*?\"[ \t]*(?P<text>[\s\S]*?)\}\}\}',
+    'folding': r'\{\{\{\#\!folding[ \t\n]+(?P<text>[\s\S]*?)\}\}\}', # rigvedawiki syntax
+    'plaintext': r'\{\{\{(?P<text>[\s\S]*?)\}\}\}',
+
+    'comment': r'\#\#.*',
+    'redirect': r'\#(?:redirect|넘겨주기)[ \t\n]+.*',
+
+    'footnote': [
+        r'\[\*[^ \t]+\]', 
+        r'\[\*[^ \t]*[ \t]+(?P<text>[^\[\]]*)\]',
+    ],
+}
+
+_priority = (
+    'macro',
+    'html', # before link
+    'image', # before link 
+    'link',
+    'horizontal_rule', # before deletion
+    'heading',
+    'bold',
+    'italic', # after bold
+    'deletion',
+    'underline',
+    'superscript',
+    'subscript',
+    'text_size',
+    'text_color',
+    'math',
+    'text_box', # before table
+    'table', # before list
+    'unordered_list',
+    'ordered_list', 
+    'quote',
+    'syntax',
+    'wiki',
+    'folding',
+    'plaintext', # after text_size, text_color, html, syntax, wiki, and folding
+    'comment',
+    'redirect',
+    'footnote',
+)
+
+# compile regular expressions
+for name in _patterns:
+    if not isinstance(_patterns[name], list):
+        _patterns[name] = [ _patterns[name] ] 
+
+    _patterns[name] = [
+        re.compile(pattern, re.IGNORECASE | re.MULTILINE)
+        for pattern in _patterns[name]
+    ]
--- namuwiki/extractor.py
+++ namuwiki/extractor.py
@@ -1,189 +1,108 @@
 import re
-import urllib.parse
-
-_patterns = {
-    # Reference: https://namu.wiki/w/%EB%82%98%EB%AC%B4%EC%9C%84%ED%82%A4:%EB%AC%B8%EB%B2%95%20%EB%8F%84%EC%9B%80%EB%A7%90
-    'macro': [
-        r'\[\[br\]\]',
-        r'\[\[(?:date|datetime|include|tableofcontents|목차|footnote|각주|wikicommons|youtube|nicovideo|html|navigation)(?:\([^\]]*\))?\]\]',
-        r'\[br\]',
-        r'\[(?:date|datetime|include|tableofcontents|목차|footnote|각주|wikicommons|youtube|nicovideo|html|navigation)(?:\([^\]]*\))?\]',
-    ],
-    'html': [
-        r'\{\{\{\#\!html[ \t\n]*([\s\S]*?)\}\}\}',
-        r'\[\[html[ \t]*\(([^\)]*)\)\]\]',
-    ],
-    'link': [
-        r'\[\[(?:https?|ftp)[^ ]+?(?:[\| ]([^\]]*))?\]\]?', # URL
-        r'\[(?!\*)(?:https?|ftp)[^ ]+?(?:[\| ]([^\]]*))?\]', # URL
-        r'\[\[([a-z]+\:[^\" \n]+?)(?:[\| ]([^\]]*))?\]\]', # interwiki
-        r'\[(?!\*)([a-z]+\:[^\" \n]+?)(?:[\| ]([^\]]*))?\]', # interwiki
-        r'\[\[([a-z]+\:\".+?(?<!\\)\")(?:[\| ]?([^\]]*))?\]\]', # interwiki
-        r'\[(?!\*)([a-z]+\:\".+?(?<!\\)\")(?:[\| ]?([^\]]*))?\]', # interwiki
-        r'\[\[([^\|\#\]]+?)(?:\#[^|\]]+)?(?:\|([^\]]*))?\]\]', # document
-        r'\[(?!\*)([^\|\#\]]+?)(?:\#[^|\]]+)?(?:\|([^\]]*))?\]', # document
-        r'[a-z]+\:(?!//)([^\" \n]+)', # interwiki
-        r'[a-z]+\:(?!//)\"(.+)(?<!\\)\"', # interwiki
-    ],
-    'footnote':[
-        r'^[ \t]*\[\*[^ ]*[ \t]*([^\[\]]*)\]', # at the beginning
-        r'\[\*[^ ]*[ \t]*([^\[\]]*)\][ \t]*\.?[ \t]*$', # at the end
-        r'\[\*[^ ]*[ \t]*([^\[\]]*)\]', # in a sentence
-    ],
-    'special_markup': [
-        # deletion
-        r'^[ \t]*~~(?!~)(.*?)~~', # at the beginning
-        r'~~(?!~)(.*?)~~[ \t]*\.?[ \t]*$', # at the end
-        r'^[ \t]*--(?!-)(.*?)--', # at the beginning
-        r'--(?!-)(.*?)--[ \t]*\.?[ \t]*$', # at the end
-    ],
-    'markup': [
-        # heading
-        r'^[ \t]*={1,}[ \t]*(.*?)[ \t]*={1,}[ \t]*$',
-        # bold
-        r"'''(?!')(.*?)'''",
-        # italic
-        r"''(?!')(.*?)''",
-        # deletion
-        r'~~(?!~)(.*?)~~', # in a sentence
-        r'--(?!-)(.*?)--', # at a sentence
-        # underline
-        r'__(?!_)(.*?)__',
-        # superscript
-        r'\^\^(?!\^)(.*?)\^\^',
-        r'\^(?!\^)(.*?)\^',
-        # subscript
-        r',,(?!,)(.*?),,',
-        # horizontal ruler
-        r'-{4,10}',
-        # unordered list
-        r'^[ \t]*\*[ \t]*(.*?)[ \t]*$',
-        # ordered list
-        r'^[ \t]*1\.[ \t]*(?:\#\d+)?[ \t]*(.*?)[ \t]*$',
-        r'^[ \t]*A\.[ \t]*(?:\#\d+)?[ \t]*(.*?)[ \t]*$',
-        r'^[ \t]*a\.[ \t]*(?:\#\d+)?[ \t]*(.*?)[ \t]*$',
-        r'^[ \t]*I\.[ \t]*(?:\#\d+)?[ \t]*(.*?)[ \t]*$',
-        r'^[ \t]*i\.[ \t]*(?:\#\d+)?[ \t]*(.*?)[ \t]*$',
-        # text size
-        r'\{\{\{\+\d+[ \t]*(.*?)[ \t]*\}\}\}',
-        # text color
-        r'\{\{\{\#(?:[0-9a-f]{6}|[a-z]+)[ \t]*(.*?)[ \t]*\}\}\}',
-        # image
-        r'(?:https?|ftp)?.*\.(?:png|gif|jpeg|jpg)(?:\?|&(?!>amp;))?.*\b',
-        # attachment
-        r'attachment:[ \t]*(?:[^ ]*\.(?:png|gif|jpeg|jpg)(?:\?|&(?!>amp;))?.*\b|".*\.(?:png|gif|jpeg|jpg)(?:\?|&(?!>amp;))?.*")',
-        # table
-        r'\|\|(?:<[^>]*>)*([^|\n]*)',
-        r'\{\{\|([\s\S]*?)\|\}\}',
-        # quote
-        r'\>(.*)',
-        # plain text
-        r'\{\{\{([\s\S]*?)\}\}\}',
-        # math
-        r'\$.*?\$',
-        # title replacement
-        r'\{\{\{[ \t]*\#title[ \t]*.*?\}\}\}',
-        # redirect
-        r'\#redirect[ \t]*.*',
-        # comment
-        r'\#\#.*',
-    ],
-}
-
-for name in _patterns:
-    _patterns[name] = list(map(lambda pattern: re.compile(pattern, re.IGNORECASE | re.MULTILINE), _patterns[name]))
+from ._syntax import _patterns, _priority
 
 def _strip_tags(source):
-    return re.sub(r'<[^<]+?>', '', source)
+    return re.sub(r'</?[^>]+>', '', source)
 
-def _clean_macro(content):
-    content = _patterns['macro'][0].sub('\n', content)
-    for pattern in _patterns['macro'][1:]:
-        content = pattern.sub('', content)
-
-    return content
-
-def _clean_html(content):
-    for pattern in _patterns['html']:
-        content = pattern.sub(lambda match: match.groups('')[0], content)
-
-    return _strip_tags(content)
-
-def _clean_link(content):
+def _strip_default(patterns, source):
     def replacement(match):
-        groups = match.groups('')
-        if len(groups) < 2:
-            replacement = groups[0]
-        else:
-            replacement = groups[1] or groups[0]
+        # extra whitespaces will be removed
+        return ' {text} '.format(text=match.groupdict().get('text', ''))
 
-        if replacement:
-            if replacement == '../':
-                replacement = ''
+    for pattern in patterns:
+        source = pattern.sub(replacement, source)
 
-            replacement = replacement.replace('/', ' ')
+    return source
 
-        return urllib.parse.unquote_plus(replacement)
+def _strip_macro(patterns, source):
+    for pattern in patterns:
+        source = pattern.sub(r' ', source)
 
-    for pattern in _patterns['link']:
-        content = pattern.sub(replacement, content)
+    return source
 
-    return content
-
-def _clean_footnote(content):
-    footnotes = []
+def _strip_html(patterns, source):
     def replacement(match):
-        footnotes.append(match.group(1))
+        return _strip_tags(match.group('text'))
+
+    for pattern in patterns:
+        source = pattern.sub(replacement, source)
+
+    return source
+
+def _strip_inline(patterns, source):
+    def replacement(match):
+        # extra whitespaces will be removed
+        return match.groupdict().get('text', '')
+
+    for pattern in patterns:
+        source = pattern.sub(replacement, source)
+
+    return source
+
+_strip_link = _strip_inline
+_strip_bold = _strip_inline
+_strip_italic = _strip_inline
+_strip_underline = _strip_inline
+_strip_superscript = _strip_inline
+_strip_subscript = _strip_inline
+_strip_text_size = _strip_inline
+_strip_text_color = _strip_inline
+
+def _strip_deletion(patterns, source):
+    items = []
+    def replacement(match):
+        items.append(match.group('text'))
         return ''
 
-    for pattern in _patterns['footnote']:
-        content = pattern.sub(replacement, content)
+    for pattern in patterns:
+        source = pattern.sub(replacement, source)
 
-    content += '\n'
-    content += '\n'.join(footnotes)
-    return content
+    if items:
+        source += '\n'
+        source += '\n'.join(items)
 
-def _clean_special_markup(content):
-    for pattern in _patterns['special_markup']:
-        content = pattern.sub(lambda match: '\n' + match.group(1) + '\n', content)
+    return source
 
-    return content
+def _strip_footnote(patterns, source):
+    def _do_strip_footnote(source):
+        items = []
+        def replacement(match):
+            items.append(match.groupdict().get('text', ''))
+            return ''
 
-def _clean_markup(content):
-    def replacement(match):
-        groups = match.groups('')
-        return ' ' + groups[0] + ' ' if groups else ''
+        for pattern in patterns:
+            source = pattern.sub(replacement, source)
 
-    for pattern in _patterns['markup']:
-        content = pattern.sub(replacement, content)
-    return content
+        if items:
+            source += '\n'
+            source += '\n'.join(items)
 
-def _clean_whitespace(content):
-    def replacement(match):
-        return match.group(1)
+        return source
 
-    # strip whitespace line by line
-    content = re.sub(r'^[ \t]*(.*?)[ \t]*$', replacement, content, flags=re.MULTILINE)
+    # to handle nested footnotes
+    previous_source = None
+    while previous_source != source:
+        previous_source = source
+        source = _do_strip_footnote(source)
 
-    # remove repeated whitespaces
-    previous_content = content
-    while True:
-        content = re.sub(r'(\s)\1', replacement, content)
-        if previous_content == content:
-            break
+    return source
 
-        previous_content = content
+def _clean_whitespace(source):
+    # strip whitespaces line by line
+    source = re.sub(r'^[ \t]*(.*?)[ \t]*$', r'\1', source, flags=re.MULTILINE)
+    
+    # remove duplicated whitespaces
+    source = re.sub(r'(\s)\1+', r'\1', source)
 
-    return content
+    return source
 
-def extract_text(content):
-    text = _clean_macro(content)
-    text = _clean_html(text)
-    text = _clean_link(text)
-    text = _clean_footnote(text)
-    text = _clean_special_markup(text)
-    text = _clean_markup(text)
-    text = _clean_whitespace(text)
-    text = text.strip()
+def extract_text(source):
+    environment = globals()
 
-    return text
+    for item in _priority:
+        name = '_strip_{item}'.format(item=item)
+        strip = _strip_default if name not in environment else environment[name]
+
+        source = strip(_patterns[item], source)
+
+    return _clean_whitespace(source)
