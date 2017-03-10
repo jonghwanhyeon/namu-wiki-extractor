43b921f336ff1f5775b4afe340270be761ca3293
Jonghwan Hyeon <hyeon0145@gmail.com>
2017-03-10 17:16:37 +0900

    Version 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='namu-wiki-extractor',
-    version='0.2.0',
+    version='0.2.1',
     description='A library to extract plaintexts from the namu wiki dump',
     url='https://github.com/hyeon0145/namu-wiki-extractor',
     author='Jonghwan Hyeon',
